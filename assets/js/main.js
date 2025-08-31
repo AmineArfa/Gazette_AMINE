29b8b61e4b2b01652e9ba9ff9d0c053fb43275a7
AmineArfa <47271974+AmineArfa@users.noreply.github.com>
2025-08-31 15:39:28 -0400

    back to source

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-
-// Basic theme JavaScript
-document.addEventListener('DOMContentLoaded', function() {
-    // Add any theme-specific JavaScript here
-    console.log('Industrial Intelligence Daily theme loaded');
-});
